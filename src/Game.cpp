3e74ce69344b2884e2b150cde3f00914612ec92c
Justin Cromer <justincromer@protonmail.com>
2024-03-31 18:50:07 -0700

    Added main game loop with fps delay

--- src/Game.cpp
+++ src/Game.cpp
@@ -15,12 +15,17 @@ void Game::Initialize() {
 		return;
 	}
 
+	SDL_DisplayMode displayMode;
+	SDL_GetCurrentDisplayMode(0, &displayMode);
+	windowWidth = 800; //displayMode.w;
+	windowHeight = 600; //displayMode.h;
+
 	window = SDL_CreateWindow(
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
+		windowWidth,
+		windowHeight,
 		SDL_WINDOW_BORDERLESS
 	);
 
@@ -29,14 +34,22 @@ void Game::Initialize() {
 		return;
 	}
 
-	renderer = SDL_CreateRenderer(window, -1, 0);
+	renderer = SDL_CreateRenderer(
+		window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+	);
+
 	if (!renderer) {
 		std::cerr << "Error creating SDL renderer." << std::endl;
 		return;
 	}
+
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+	running = true;
 }
 
 void Game::Run() {
+	Setup();
 	while (running) {
 		ProcessInput();
 		Update();
@@ -60,15 +73,45 @@ void Game::ProcessInput() {
 	}
 }
 
+glm::vec2 playerPosition;
+glm::vec2 playerVelocity;
+
+void Game::Setup() {
+	playerPosition = glm::vec2(10.0, 20.0);
+	playerVelocity = glm::vec2(1.0, 0.0);
+}
+
 void Game::Update() {
+	int timeToWait = MILLISECONDS_PER_FRAME - (SDL_GetTicks() - previousFrameMs);
+	if (timeToWait > 0 && timeToWait <= MILLISECONDS_PER_FRAME) {
+		SDL_Delay(timeToWait);
+	}
+
+	// If we are too fast, waste time until we reach target FPS
+	previousFrameMs = SDL_GetTicks();
+
+	playerPosition.x += playerVelocity.x;
+	playerPosition.y += playerVelocity.y;
 }
 
 void Game::Render() {
-	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 128);
+	SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
 	SDL_RenderClear(renderer);
 
-	// TODO: Render game objects
+	// Draw a PNG texture
+	SDL_Surface* surface = IMG_Load("./assets/images/tank-tiger-right.png");
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_FreeSurface(surface);
 
+	// Destination rectangle that we want to place our texture
+	SDL_Rect dstRect = { 
+		static_cast<int>(playerPosition.x),
+		static_cast<int>(playerPosition.y),
+		32,
+		32
+	};
+	SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+	SDL_DestroyTexture(texture);
 	SDL_RenderPresent(renderer);
 }
 
--- src/Game.h
+++ src/Game.h
@@ -1,20 +1,30 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <glm/glm.hpp>
 #include <iostream>
+
+static constexpr int FPS = 30;
+static constexpr int MILLISECONDS_PER_FRAME = 1'000 / FPS;
 
 class Game {
 private:
 	bool running = false;
 	SDL_Window* window;
 	SDL_Renderer* renderer;
+	int previousFrameMs;
 
 public:
 	Game();
 	~Game();
+	void Setup();
 	void Initialize();
 	void Run();
 	void ProcessInput();
 	void Update();
 	void Render();
 	void Destroy();
+
+	size_t windowWidth;
+	size_t windowHeight;
 };
